c0d63feab294b3a606c820ae83708d8cce962164
PranavKrishnaa <81923726+PranavKrishnaa@users.noreply.github.com>
2021-06-04 12:06:07 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -23,7 +23,7 @@ function setup() {
   stand1 = new Stand(390,300,250,10);
   stand2 = new Stand(700,200,200,10);
  
-  //level one
+  //level one - First Tower
   block1 = new Block(300,275,30,40);
   console.log(block1);
   block2 = new Block(330,275,30,40);
@@ -32,18 +32,33 @@ function setup() {
   block5 = new Block(420,275,30,40);
   block6 = new Block(450,275,30,40);
   block7 = new Block(480,275,30,40);
-  //level two
+  //level two - First Tower
   block8 = new Block(330,235,30,40);
   block9 = new Block(360,235,30,40);
   block10 = new Block(390,235,30,40);
   block11 = new Block(420,235,30,40);
   block12 = new Block(450,235,30,40);
-  //level three
+  //level three - First Tower
   block13 = new Block(360,195,30,40);
   block14 = new Block(390,195,30,40);
   block15 = new Block(420,195,30,40);
-  //top
+  //top - First Tower
   block16 = new Block(390,155,30,40);
+
+  //level1 - Second Tower
+  block17 = new Block(640,175,30,40);
+  block18 = new Block(670,175,30,40);
+  block19 = new Block(700,175,30,40);
+  block20 = new Block(730,175,30,40);
+  block21 = new Block(760,175,30,40);
+  
+  //level2 - Second Tower
+  block22 = new Block(670,135,30,40);
+  block23 = new Block(700,135,30,40);
+  block24 = new Block(730,135,30,40);
+
+  //top - Second Tower
+  block25 = new Block(700,95,30,40);
 
   polygon = Bodies.circle(50,200,20);
   World.add(world,polygon);
@@ -61,10 +76,12 @@ function draw() {
 
   ground.display();
   stand1.display();
+  stand2.display();
   
   strokeWeight(2);
   stroke(15);
   fill("skyblue");
+  //level1 - First Tower
   block1.display();
   block2.display();
   block3.display();
@@ -72,18 +89,38 @@ function draw() {
   block5.display();
   block6.display();
   block7.display();
+
+  //level1 - Second Tower
+  block17.display();
+  block18.display();
+  block19.display();
+  block20.display();
+  block21.display();
+
   fill("pink");
+  //level2 - First Tower
   block8.display();
   block9.display();
   block10.display();
   block11.display();
   block12.display();
+
+  //level2 - Second Tower
+  block22.display();
+  block23.display();
+  block24.display();
+
   fill("turquoise");
+  //level three - First Tower
   block13.display();
   block14.display();
   block15.display();
   fill("grey");
+  //top - First Tower
   block16.display();
+  
+  //top - Second Tower
+  block25.display();
  
   slingShot.display();
 
